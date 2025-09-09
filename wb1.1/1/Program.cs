5316d3eac4fdb702d5b66a84b7f9ec590a57ff0b
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-09-09 18:41:23 +0300

    Do wb1.2

---
+++ wb1.1/1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    internal class Program
+    {
+        static bool IsWeekend(uint day, uint dayOfTheWeek)
+        {
+            dayOfTheWeek = dayOfTheWeek % 7; // Must be between 0 and 7
+            int offset = (int)(-dayOfTheWeek); // Offset puts weekend closer, so it's negative
+
+            bool isWeekday = false;
+            if ((day == 7 + offset || day == 8 + offset) ||
+                (day == 14 + offset || day == 15 + offset) ||
+                (day == 21 + offset || day == 22 + offset) ||
+                (day == 28 + offset || day == 29 + offset))
+            {
+                isWeekday = true;
+            }
+            bool isHoliday = day <= 5 && day >= 1 || day >= 8 && day <= 10;
+            
+            return isHoliday || isWeekday;
+        }
+        static void Main()
+        {
+            do
+            {
+                Console.Write("Введите номер дня недели, с которого начинается месяц (1-пн,...7-вс): ");
+                uint dayOfTheWeek = Convert.ToUInt32(Console.ReadLine());
+                
+                Console.Write("Введите день месяца: ");
+                uint day = Convert.ToUInt32(Console.ReadLine());
+
+                if (day == 0 || dayOfTheWeek == 0)
+                {
+                    break;
+                }
+                else {
+                    Console.WriteLine("-----Проверяем выходной ли день-----");
+                    if (IsWeekend(day,dayOfTheWeek))
+                    {
+                        Console.WriteLine("Выходной день");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Рабочий день");
+                    }
+                }
+            } while(true);
+        }
+    }
+}
---
+++ wb1.1/2/Program.cs
@@ -0,0 +1,45 @@
+﻿void PrintCountOfBucks(long sum)
+{
+    if (sum % 100 != 0)
+    {
+        Console.WriteLine($"Выдать {sum} рублей невозможно - сумма не кратна стам.");
+        return;
+    }
+
+    long cycles = sum / 150000; // Can't give more than 150000 at once
+    sum -= cycles * 150000;
+
+    long fiveThousand = sum / 5000 + cycles * 30;
+    sum %= 5000;
+
+    long oneThousand = sum / 1000;
+    sum %= 1000;
+
+    long fiveHundred = sum / 500;
+    sum %= 500;
+
+    long twoHundred = sum / 200;
+    sum %= 200;
+
+    long oneHundred = sum / 100;
+
+    Console.WriteLine($"""
+        Количество прогонов по 150к: {cycles}
+        5000: {fiveThousand}
+        1000: {oneThousand}
+        500: {fiveHundred}
+        200: {twoHundred}
+        100: {oneHundred}
+    """);
+}
+
+do
+{
+    Console.Write("Введите вашу сумму: ");
+    long sum = Convert.ToInt64(Console.ReadLine());
+    PrintCountOfBucks(sum);
+
+    Console.Write("Продолжить работу? (y/n) ");
+    char isContinue = Convert.ToChar(Console.ReadLine());
+    if (isContinue == 'n') break;
+} while (true);
